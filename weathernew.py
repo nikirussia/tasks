e05e064934dc3c7c871669ad59c1d955ef288dcb
Nikirussia <nikirussia94@gmail.com>
2019-04-27 19:03:32 +0300

    first commit

---
+++ weathernew.py
@@ -0,0 +1,20 @@
+import pyowm
+
+owm = pyowm.OWM('6bed291744d74f51ca0b57e6024304c0', language = "ru")
+
+place = input("Какой город интересует?: ")
+
+observation = owm.weather_at_place(place)
+w = observation.get_weather()
+
+temp = w.get_temperature('celsius')["temp"]
+
+print( "В городе " + place + " сейчас " + w.get_detailed_status())
+print("Температура сейчас в районе " + str(temp))
+
+if temp < 5:
+    print( "Не выходи из дома, не совершай ошибок")
+elif temp < 10:
+    print("Прежде чем выйти - оденься")
+else:
+    print("Иди смело")
